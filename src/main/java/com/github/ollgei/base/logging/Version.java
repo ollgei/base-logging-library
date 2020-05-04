56b7a05b3fb37a871906424cad04cc90397cefde
ollgei <ollgei@sina.com>
2020-05-04 13:09:05 +0800

    fix: init project

---
+++ src/main/java/com/github/ollgei/base/logging/Version.java
@@ -0,0 +1,12 @@
+package com.github.ollgei.base.logging;
+
+/**
+ * version.
+ *
+ * @author jiawei
+ * @since 1.0.0
+ */
+public class Version {
+
+    public static final String VERSION1 = "2020.1.0.SR1";
+}
